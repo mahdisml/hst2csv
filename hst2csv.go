ae9d33733b2a62148c0fff1a4a9fc59e7c3ad5ee
Barry Hoyte <4843960+IntelliDrone@users.noreply.github.com>
2021-02-21 16:39:36 +0000

    Update hst2csv.go

--- hst2csv.go
+++ hst2csv.go
@@ -135,7 +135,8 @@ func ParseHistoryOld(file *os.File) (hst HistoricalBytes) {
 }
 
 func CreateCsvFile(string ArgsFileName) String() string {
-	var string CsvFile, FileName = ""
+	var string FileName = ""
+	var string CsvFile = ""
 	const string Directory = "/src/csv/"
 	FileName  = ArgsFileName
 	CsvFile := strings.Replace(FileName, ".hst", ".csv", 1)
